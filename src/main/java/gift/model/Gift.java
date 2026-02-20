2c4be91f33d97015629153b8e512d8f48b0f5443
문정환(malang.cow) <bazzi2548@naver.com>
2026-02-20 15:24:31 +0900

    feat: 사전 구현 1번 - Gift.java: POJO → Entity (from/to는 SQL 예약어이므로 sender/receiver로 변경, @ManyToOne 관계 설정) - GiftRepository.java: 신규 생성 - GiftService.java: MemberRepository + GiftRepository 주입, Gift 저장 - FakeGiftDelivery.java: Gift가 Member를 직접 참조하므로 MemberRepository 제거

--- src/main/java/gift/model/Gift.java
+++ src/main/java/gift/model/Gift.java
@@ -1,26 +1,55 @@
 package gift.model;
 
-public class Gift {
-    private final Long from;
-    private final Long to;
-    private final Option option;
-    private final int quantity;
-    private final String message;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
-    public Gift(final Long from, final Long to, final Option option, final int quantity, final String message) {
-        this.from = from;
-        this.to = to;
+@Entity
+public class Gift {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member sender;
+
+    @ManyToOne
+    private Member receiver;
+
+    @ManyToOne
+    private Option option;
+
+    private int quantity;
+    private String message;
+
+    protected Gift() {
+    }
+
+    public Gift(final Member sender, final Member receiver, final Option option, final int quantity, final String message) {
+        this(null, sender, receiver, option, quantity, message);
+    }
+
+    public Gift(final Long id, final Member sender, final Member receiver, final Option option, final int quantity, final String message) {
+        this.id = id;
+        this.sender = sender;
+        this.receiver = receiver;
         this.option = option;
         this.quantity = quantity;
         this.message = message;
     }
 
-    public Long getFrom() {
-        return from;
+    public Long getId() {
+        return id;
     }
 
-    public Long getTo() {
-        return to;
+    public Member getSender() {
+        return sender;
+    }
+
+    public Member getReceiver() {
+        return receiver;
     }
 
     public Option getOption() {
