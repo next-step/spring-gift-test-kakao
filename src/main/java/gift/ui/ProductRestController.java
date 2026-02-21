c753ecc14a66c51b7bb51cac9b91558188b2a95f
jbw9964 <jbw9964@gmail.com>
2026-02-21 22:12:07 +0900

    feat: 요청 validation 을 위한 계약 추가 및 endpoint request validation 추가

--- src/main/java/gift/ui/ProductRestController.java
+++ src/main/java/gift/ui/ProductRestController.java
@@ -1,15 +1,19 @@
 package gift.ui;
 
-import gift.application.CreateProductRequest;
 import gift.application.ProductService;
+import gift.application.request.CreateProductRequest;
 import gift.model.Product;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/products")
 @RestController
 public class ProductRestController {
+
     private final ProductService productService;
 
     public ProductRestController(final ProductService productService) {
@@ -18,6 +22,8 @@ public class ProductRestController {
 
     @PostMapping
     public Product create(@RequestBody final CreateProductRequest request) {
+        request.validate();
+
         return productService.create(request);
     }
 
