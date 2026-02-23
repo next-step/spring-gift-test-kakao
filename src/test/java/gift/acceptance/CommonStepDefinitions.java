43ea457552894fcea403d3be98fff175d929df51
seungwone <aahhll654@gmail.com>
2026-02-23 16:43:33 +0900

    test: cucumber를 이용해 카테고리 테스트 작성

---
+++ src/test/java/gift/acceptance/CommonStepDefinitions.java
@@ -0,0 +1,58 @@
+package gift.acceptance;
+
+import gift.support.DatabaseCleanup;
+import io.cucumber.java.Before;
+import io.cucumber.java.ko.그러면;
+import io.cucumber.java.ko.그리고;
+import io.restassured.RestAssured;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class CommonStepDefinitions {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @Autowired
+    private AcceptanceContext context;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+        databaseCleanup.execute();
+    }
+
+    @그러면("응답 상태 코드는 {int}이다")
+    public void 응답_상태_코드는_이다(int statusCode) {
+        assertThat(context.getResponse().statusCode(), equalTo(statusCode));
+    }
+
+    @그리고("응답의 {string} 필드는 비어있지 않다")
+    public void 응답의_필드는_비어있지_않다(String fieldName) {
+        assertThat(context.getResponse().jsonPath().get(fieldName), notNullValue());
+    }
+
+    @그리고("응답의 {string} 필드는 {string}이다")
+    public void 응답의_필드는_이다(String fieldName, String expectedValue) {
+        assertThat(context.getResponse().jsonPath().getString(fieldName), equalTo(expectedValue));
+    }
+
+    @그리고("응답 목록의 {string} 필드에 {string}이 포함되어 있다")
+    public void 응답_목록의_필드에_이_포함되어_있다(String fieldName, String expectedValue) {
+        assertThat(context.getResponse().jsonPath().getList(fieldName, String.class), hasItem(expectedValue));
+    }
+
+    @그리고("응답은 빈 목록이다")
+    public void 응답은_빈_목록이다() {
+        assertThat(context.getResponse().jsonPath().getList("$"), empty());
+    }
+}
