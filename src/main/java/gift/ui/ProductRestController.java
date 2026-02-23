9c8f8fb89f29478fa65711c5be6ad53c88668ac5
seogi.nam <namyunseok0328@gmail.com>
2026-02-23 14:43:15 +0900

    refactor: Controller에 @RequestBody 추가

--- src/main/java/gift/ui/ProductRestController.java
+++ src/main/java/gift/ui/ProductRestController.java
@@ -3,8 +3,10 @@ package gift.ui;
 import gift.application.CreateProductRequest;
 import gift.application.ProductService;
 import gift.model.Product;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +15,19 @@ import java.util.List;
 @RequestMapping("/api/products")
 @RestController
 public class ProductRestController {
-    private final ProductService productService;
+	private final ProductService productService;
 
-    public ProductRestController(final ProductService productService) {
-        this.productService = productService;
-    }
+	public ProductRestController(final ProductService productService) {
+		this.productService = productService;
+	}
 
-    @PostMapping
-    public Product create(final CreateProductRequest request) {
-        return productService.create(request);
-    }
+	@PostMapping
+	public Product create(@RequestBody final CreateProductRequest request) {
+		return productService.create(request);
+	}
 
-    @GetMapping
-    public List<Product> retrieve() {
-        return productService.retrieve();
-    }
+	@GetMapping
+	public List<Product> retrieve() {
+		return productService.retrieve();
+	}
 }
