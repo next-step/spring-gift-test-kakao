e1ed128c8a13c406229f186273683b586ed0a753
revan.lib <revan.lib@kakaocorp.com>
2026-02-24 15:06:09 +0900

    chore: Docker 컨테이너 E2E 테스트 모드 추가
    -Pmode=docker 옵션으로 앱을 Docker 컨테이너에서 실행하여 테스트 가능.
    호스트에서 bootJar 빌드 후 이미지에 복사하는 방식으로 Alpine ARM64 호환성 확보.

--- src/test/java/gift/cucumber/hooks/DataCleanupHook.java
+++ src/test/java/gift/cucumber/hooks/DataCleanupHook.java
@@ -6,11 +6,13 @@ import gift.model.OptionRepository;
 import gift.model.ProductRepository;
 import io.cucumber.java.Before;
 import io.restassured.RestAssured;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 public class DataCleanupHook {
 
     private final int port;
+    private final String targetBaseUrl;
     private final OptionRepository optionRepository;
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
@@ -18,12 +20,14 @@ public class DataCleanupHook {
 
     public DataCleanupHook(
         @LocalServerPort final int port,
+        @Value("${test.target.base-url:}") final String targetBaseUrl,
         final OptionRepository optionRepository,
         final ProductRepository productRepository,
         final CategoryRepository categoryRepository,
         final MemberRepository memberRepository
     ) {
         this.port = port;
+        this.targetBaseUrl = targetBaseUrl;
         this.optionRepository = optionRepository;
         this.productRepository = productRepository;
         this.categoryRepository = categoryRepository;
@@ -32,7 +36,13 @@ public class DataCleanupHook {
 
     @Before
     public void setUp() {
-        RestAssured.port = port;
+        if (targetBaseUrl != null && !targetBaseUrl.isBlank()) {
+            RestAssured.baseURI = targetBaseUrl;
+            RestAssured.port = RestAssured.UNDEFINED_PORT;
+        } else {
+            RestAssured.baseURI = "http://localhost";
+            RestAssured.port = port;
+        }
         optionRepository.deleteAll();
         productRepository.deleteAll();
         categoryRepository.deleteAll();
